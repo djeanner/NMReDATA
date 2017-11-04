258f8fb4a44fad09cda897f201e9072878f48075
shk3 <stefan.kuhn@uni-koeln.de>
2017-11-04 22:22:56 +0100

    initial checking

---
+++ tools/checknmredata/src/test/de/unikoeln/chemie/nmr/test/NmredataReaderTest.java
@@ -0,0 +1,26 @@
+package de.unikoeln.chemie.nmr.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.openscience.cdk.exception.CDKException;
+
+import de.unikoeln.chemie.nmr.data.NmreData;
+import de.unikoeln.chemie.nmr.io.NmredataReader;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class NmredataReaderTest  extends TestCase{
+	
+	public void testSimple() throws Exception, IOException{
+		String filename = "testdata/example.sd";
+        InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+        NmredataReader reader = new NmredataReader(ins);
+        NmreData data = reader.read();
+        Assert.assertEquals(32, data.getMolecule().getAtomCount());
+        Assert.assertEquals(36, data.getMolecule().getBondCount());
+        Assert.assertEquals(1, data.getSpectra().size());
+        Assert.assertEquals(7, data.getSpectra().get(0).getPeakTable().length);
+	}
+	
+}
